f99ba86f3ea4f5747672daff4009cf80fc952841
Anna Clara <anisferraz@gmail.com>
2021-07-16 21:48:48 -0300

    Add logout

--- src/lib/AlurakutCommons.js
+++ src/lib/AlurakutCommons.js
@@ -35,7 +35,7 @@ export function AlurakutMenu({ githubUser }) {
                 </nav>
 
                 <nav>
-                    <a href={`/logout`}>
+                    <a href={`/login`}>
                         Sair
           </a>
                     <div>
@@ -212,7 +212,7 @@ export function AlurakutProfileSidebarMenuDefault() {
                     <img src={`${BASE_URL}/icons/plus.svg`} />
             GitHub Trends
           </a>
-                <a href="/logout">
+                <a href="/login">
                     <img src={`${BASE_URL}//icons/logout.svg`} />
             Sair
           </a>
